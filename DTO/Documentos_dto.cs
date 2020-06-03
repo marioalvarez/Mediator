578e6bfabfb2b5ad67e4e20b01df586c18395f38
Mario Alvarez Zapata <marioalvarez.informatico@gmail.com>
2020-06-03 19:26:45 -0400

    agrega clases a DTO

---
+++ DTO/Documentos_dto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class Documentos_dto
+    {
+        public int Id_id { get; set; }
+        public string Tp_descripcion { get; set; }
+        public string Estado { get; set; }
+    }
+}
---
+++ DTO/Mediaciones_dto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class Mediaciones_dto
+    {
+        public int Id { get; set; }
+        public string Folio { get; set; }
+        public int Id_id { get; set; }
+        public DateTime Fecha_doc { get; set; }
+        public string MIME_TYPE { get; set; }
+        public string FILE_NAME { get; set; }
+        public Byte[] FILE_DATA { get; set; }
+        public string FILE_PATH { get; set; }
+        public string Nota { get; set; }
+        public int FILE_SIZE { get; set; }
+        public string Num_doc { get; set; }
+        public int Estado { get; set; }
+        public string Visibilidad { get; set; }
+        public DateTime Fecha_creacion { get; set; }
+        public string Otro_doc { get; set; }
+        public int FILE_NUM_PAG { get; set; }
+        public string Tp_descripcion { get; set; } //REF: Tabla tipo documentos
+        public int Cod_empresa { get; set; }
+        public string Usuario_rut { get; set; }
+        public string Nombre_completo { get; set; }
+    }
+}
