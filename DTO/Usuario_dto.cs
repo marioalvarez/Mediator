aa491c056b14a71b87abae4a017472dc968597e6
Mario Alvarez Zapata <marioalvarez.informatico@gmail.com>
2020-06-03 18:39:52 -0400

    agregando clases

---
+++ DTO/Usuario_dto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class Usuario_dto
+    {
+        public string Rut { get; set; }
+        public string Nombre { get; set; }
+        public string APaterno { get; set; }
+        public string AMaterno { get; set; }
+        public string Password { get; set; }
+        public string Perfil { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Estado { get; set; }
+    }
+}
